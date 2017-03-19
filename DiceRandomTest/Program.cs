8380ddae2156a510b5de018824db8524fee96bb7
stevieray8450 <stevieray8450@STEPHENBONID14B>
2017-03-18 21:12:44 -0500

    Add project files.

---
+++ DiceRandomTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
